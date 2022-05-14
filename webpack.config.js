e1abe748b822eeef7b64c6a2d5b1d2dc06397e62
PHC <lucidsun92@gmail.com>
2022-05-14 23:19:34 +0900

    add babel module to webpackconfig

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,37 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  mode: "development"
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /[\.js]$/, // .js에 한하여 babel-loader를 이용하여 transpiling
+                exclude: /node_module/,
+                use: {
+                    loader: "babel-loader",
+                },
+            },
+            {
+                test: /\.ts$/, // .ts에 한하여 ts-loader를 이용하여 transpiling
+                exclude: /node_module/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            ['@babel/preset-typescript', { allowNamespaces: true }]
+                        ]
+                    }
+                },
+            },
+        ],
+    },
+    resolve: {
+        modules: [path.join(__dirname, "src"), "node_modules"], // 모듈 위치
+        extensions: [".ts", ".js"],
+    },
+    mode: "development"
 };
